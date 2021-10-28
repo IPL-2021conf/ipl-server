ece2691423ae4ae96be0ddab12c658db6777a61c
YiJoonYong <yijy001@gmail.com>
2021-10-28 23:57:27 +0900

    211028 add db

--- ipl_project/settings.py
+++ ipl_project/settings.py
@@ -213,6 +213,6 @@ AWS_S3_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 # Heroku: Update database configuration from $DATABASE_URL.
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
