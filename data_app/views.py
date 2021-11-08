35fbd270d721287d4bfe3c0b0de81c75f99b6f05
YiJoonYong <yijy001@gmail.com>
2021-11-08 22:31:31 +0900

    211108 view order add

--- data_app/views.py
+++ data_app/views.py
@@ -5,7 +5,7 @@ from rest_framework.filters import SearchFilter
 from rest_framework_simplejwt.tokens import AccessToken
 # Create your views here.
 class VideoViewSet(viewsets.ModelViewSet):
-    queryset = VideoPostModel.objects.all()
+    queryset = VideoPostModel.objects.all().order_by('-date')
     serializer_class = VideoPostSerilizer
     
     filter_backends = [SearchFilter]
@@ -29,7 +29,7 @@ class VideoViewSet(viewsets.ModelViewSet):
 from .serializers import ImagePostSerializer
 from .models import ImagePostModel
 class ImageViewSet(viewsets.ModelViewSet):
-    queryset = ImagePostModel.objects.all()
+    queryset = ImagePostModel.objects.all().order_by('-date')
     serializer_class = ImagePostSerializer
 
     filter_backends = [SearchFilter]
@@ -41,6 +41,7 @@ class ImageViewSet(viewsets.ModelViewSet):
         email = access_token.payload.get('email')
         print('email: ', email)
         images = super().get_queryset()
+        
         if self.request.user.is_authenticated:  #인증받은 사용자가 아니면 목록을 보이지 않음
             pass
         else:
